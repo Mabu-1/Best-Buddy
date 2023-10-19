663605218a8522d1f8f34b8816f779365027518a
Mabu1 <umahtab65@gmail.com>
2023-10-19 08:27:38 +0600

    initila project has been created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRZegUnvEyTHdYpBX48DltJI86OSfd4vs",
+  authDomain: "best-buddy-a42a3.firebaseapp.com",
+  projectId: "best-buddy-a42a3",
+  storageBucket: "best-buddy-a42a3.appspot.com",
+  messagingSenderId: "926565074924",
+  appId: "1:926565074924:web:8ab930ad8a1f9523d622c9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
